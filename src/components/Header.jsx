c3b22dd5ce49574e3ab6fc9d1c9107be16ca9072
ShaheerAhmedKhan11 <shaheerahmedkhandev@gmail.com>
2025-05-31 13:41:30 +0500

    crreated

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header(){
+    return(
+        <header className="d-flex justify-content-center p-4 shadow bg-white">
+            <img  src="/src/assets/ChefClaude.png" alt="Chef Claude" />
+            <h1>Chef Claude</h1>
+        </header>
+    );
+}
