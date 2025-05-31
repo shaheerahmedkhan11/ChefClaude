8ab0017ec0164f5d8492a091d9505a27ac1f23a6
ShaheerAhmedKhan11 <shaheerahmedkhandev@gmail.com>
2025-05-31 16:53:13 +0500

    updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 export default function Header(){
     return(
-        <header className="d-flex justify-content-center p-4 shadow bg-white">
+        <header className="d-flex justify-content-center p-4 mb-5 shadow bg-white">
             <img  src="/src/assets/ChefClaude.png" alt="Chef Claude" />
             <h1>Chef Claude</h1>
         </header>
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,8 @@
 export default function Main(){
     return (
-        <main>
-         
+        <main className="d-flex justify-content-center m-5">
+          <input type="text" className="form-control p-2" placeholder="e.g. oregano"></input>
+          <button className="btn btn-dark ms-3">+ Add Ingredients</button>
         </main>
     );
 }
