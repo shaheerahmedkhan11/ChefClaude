78ff4c867af894c383fa3280cf416b2410b8afa7
ShaheerAhmedKhan11 <shaheerahmedkhandev@gmail.com>
2025-06-02 13:59:08 +0500

    refactor: restructure App and Main components for improved functionality and user interaction

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import Header from "./components/Header";
 import Main from "./components/Main";
+import React from "react";
 
 export default function App() {
+
+  const [response,setResponse] = React.useState("Yes!");
+
+  function handleClick() {
+    setResponse("Definitely!");
+  }
+
   return (
     <>
       <Header />
       <Main />
+      <div className="container m-5">
+        <button onClick={handleClick} className="btn btn-outline-primary">{response}</button>
+      </div>
     </>
   );
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,8 +1,32 @@
-export default function Main(){
-    return (
-        <main className="d-flex justify-content-center m-5">
-          <input type="text" className="form-control p-2" placeholder="e.g. oregano"></input>
-          <button className="btn btn-dark ms-3">+ Add Ingredients</button>
-        </main>
-    );
+export default function Main() {
+  const ingredients = ["oregano", "basil", "parsley", "thyme", "cilantro"];
+  const ingredientList = ingredients.map(ingredient =>(<li key={ingredient}>{ingredient}</li>));
+
+function handleSubmit(event){
+  event.preventDefault();
+  console.log("Form submitted");
+  const formData = new FormData(event.currentTarget);
+  const ingredient = formData.get("ingredient");
+  console.log(ingredient);
+  ingredients.push(ingredient);
+  console.log(ingredients);
+}
+
+  return (
+    <main className="container">
+      <form onSubmit={handleSubmit} className="m-5 d-flex justify-content-center">
+        <input
+          type="text"
+          className="form-control p-2 me-2"
+          placeholder="e.g. oregano"
+          aria-label="Add ingredient"
+          name="ingredient"
+        />
+        <button className="btn btn-dark">+ Add Ingredients</button>
+      </form>
+      <ul className="ms-5">
+        {ingredientList}
+      </ul>
+    </main>
+  );
 }
